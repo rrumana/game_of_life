8675f8daf172b5c0a2633627551a2536094cff65
rrumana <ryan.rumana@gmail.com>
2025-06-12 01:10:21 -0700

    Small changes while using to test reversal

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use rayon::slice::ParallelSliceMut;
 use std::io::{self, Write};
 use std::{thread, time};
 
-const WIDTH: usize = 20;
-const HEIGHT: usize = 20;
+const WIDTH: usize = 19;
+const HEIGHT: usize = 7;
 
 fn index(row: usize, col: usize) -> usize {
     row * WIDTH + col
@@ -47,7 +47,11 @@ fn parse_initial_state(initial: &[&str]) -> Vec<u8> {
     let mut grid = Vec::with_capacity(WIDTH * HEIGHT);
     for row in initial {
         for ch in row.chars() {
-            let cell = ch.to_digit(10).unwrap_or(0) as u8;
+            let cell = match ch {
+                '█' => 1,  // Black square represents alive cell
+                '⬜' => 0,  // Middle dot represents dead cell
+                _ => 0,    // Default to dead cell for any other character
+            };
             grid.push(cell);
         }
     }
@@ -73,40 +77,21 @@ fn main() {
     io::stdout().flush().unwrap();
 
     let initial_state = [
-        "00100000000000000000",
-        "10100000000000000111",
-        "01100000000000000000",
-        "00000000001100000000",
-        "00000000001100000010",
-        "00100000000000000010",
-        "10100000000000000010",
-        "01100000000000000000",
-        "00000000000000000000",
-        "00000000000000000111",
-        "00100000000000000000",
-        "10100000000000000000",
-        "01100000000000000000",
-        "00000000000000000000",
-        "00000000000000000000",
-        "00000000000000000000",
-        "00000000000000000000",
-        "00000000000000000010",
-        "00000000000000000010",
-        "00000000000000000010",
+        "⬜███⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜",
+        "⬜██⬜⬜██⬜⬜██⬜⬜███⬜██",
+        "██⬜█⬜⬜█⬜⬜⬜⬜█⬜⬜██⬜⬜⬜",
+        "⬜⬜⬜█⬜⬜██⬜█⬜⬜█⬜⬜██⬜⬜",
+        "⬜██⬜⬜█⬜█⬜⬜⬜██⬜█⬜⬜█⬜",
+        "⬜⬜███⬜⬜⬜█⬜███⬜██⬜██",
+        "⬜⬜⬜⬜⬜⬜██⬜⬜█⬜███⬜██⬜",
     ];
 
     let mut grid = parse_initial_state(&initial_state);
     let mut next_grid = vec![0u8; WIDTH * HEIGHT];
-
-    for _ in 0..10 { // simulate 10 generations
-        update(&grid, &mut next_grid);
-        std::mem::swap(&mut grid, &mut next_grid);
-    }
-
-    let frame_duration = time::Duration::from_millis(200);
+    let frame_duration = time::Duration::from_millis(400);
 
     // Main simulation loop.
-    for _ in 0..57 {
+    for _ in 0..8 {
         update(&grid, &mut next_grid);
         std::mem::swap(&mut grid, &mut next_grid);
         print!("\x1b[H");
@@ -114,6 +99,8 @@ fn main() {
         io::stdout().flush().unwrap();
         thread::sleep(frame_duration);
     }
+
+    thread::sleep(time::Duration::from_millis(5000));
 
     print!("\x1b[?1049l");
     io::stdout().flush().unwrap();
